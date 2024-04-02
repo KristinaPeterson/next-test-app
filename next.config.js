f4a8b4e76938659261a816675a68ca21f3eb0c28
Kristina Peterson <63321758+KristinaPeterson@users.noreply.github.com>
2024-04-02 12:38:50 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
-    /**
+ 
   reactStrictMode: true,
-  swcMinify: true,*/
+  swcMinify: true,
 }
 
 module.exports = nextConfig
